e2760a2c66a39ca0d7980c6afbf8a85b3623fa6c
Matheus Cesar Carneiro <matheuscesar2000@gmail.com>
2020-08-07 20:17:39 -0300

    started

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app" class="light">
+
+		<img src="./assets/wrap20.svg" alt="">
+
+		<button class="btn">aaa</button>
+		<input type="text" class="inpt" >
+
+
+		<select name="" id="" class="slct">
+			<option value="1">English</option>
+		</select>
+	</div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+	}
 
-#nav {
-  padding: 30px;
-}
+	#nav {
+		padding: 30px;
+	}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+	#nav a {
+		font-weight: bold;
+		color: #2c3e50;
+	}
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+	#nav a.router-link-exact-active {
+		color: #42b983;
+	}
 </style>
